331f6cf717cf1c1b8ab3b70581a67998c19bc930
Sahaj-Devulapally <76732777+Sahaj-Devulapally@users.noreply.github.com>
2021-03-18 14:38:32 +0530

    Add files via upload

--- Cirlce.js
+++ Cirlce.js
@@ -1,26 +1,27 @@
 class Circle{
-    constructor(x,y,radius){
+  constructor(x,y,radius){
+
   
-    
-    var options={
-       restitution:1,
-       friction:2,
-       density:4,
-       isStatic:false
-    }
-    this.body=Bodies.circle(x,y,radius,options)
-    World.add(world,this.body)
-    
-    }
-    display(){
-  var pos=this.body.position
-  var angle=this.body.angle
+  var options={
+     restitution:0.1,
+     friction:0,
+     density:0.8,
+     isStatic:false
+  }
+  this.body=Bodies.circle(x,y,radius/2,options)
+  this.radius=radius
+  World.add(world,this.body)
+  
+  }
+  display(){
+var pos=this.body.position
+
 push()
 translate(pos.x,pos.y)
-rotate(angle)
+
 ellipseMode(RADIUS)
-ellipse(0,0,this.radius,35)
+ellipse(0,0,this.radius,this.radius)
 pop()
 
-    }
-    }
+  }
+  }
